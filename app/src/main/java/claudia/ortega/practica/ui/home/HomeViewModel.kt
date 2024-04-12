615e82bbaa5e1fefad92aabfe93a7cbe7c299b25
Claudia <20230019@ricaldone.edu.sv>
2024-04-12 13:32:20 -0600

    fragmentos

---
+++ app/src/main/java/claudia/ortega/practica/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package claudia.ortega.practica.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
